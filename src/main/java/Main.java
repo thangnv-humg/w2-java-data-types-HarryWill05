ba7fe4c6f595bae7b52d4ae3825630d2537bf211
HarryWill05 <huydaothe835@gmail.com>
2025-01-16 14:33:06 +0700

    Upcode

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,19 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
-        // Ví dụ:
-        // int myInt = 42;
-        // double myDouble = 3.14;
-        // String myString = "Hello, Java!";
-        // System.out.println(myInt);
-        // System.out.println(myDouble);
-        // System.out.println(myString);
+
+        int a = 42;
+        double b = 3.14;
+        String hello = "Hello, Java!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(hello);
+
+        if (a > 10) {
+            System.out.println("Greater than 10");
+        } else {
+            System.out.println("Less or equal to 10");
+        }
+        
     }
 }
