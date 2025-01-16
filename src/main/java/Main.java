5558bfc387c33d7ae5391e043d6c804ed6fd5440
HarryWill05 <huydaothe835@gmail.com>
2025-01-16 14:40:27 +0700

    fixed code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         // TODO: Khai báo biến và viết chương trình theo yêu cầu trong README.md
 
+        // Ví dụ:
+        // int myInt = 42;
+        // double myDouble = 3.14;
+        // String myString = "Hello, Java!";
+        // System.out.println(myInt);
+        // System.out.println(myDouble);
+        // System.out.println(myString);
 
         int a = 42;
         double b = 3.14;
